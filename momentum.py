ec094beb11e9a3224f703a8f4516963a78a0171a
jjh68069 <h.jason0@gmail.com>
2015-11-10 16:55:41 -0700

    Update momentum finder file to use with automated unit testing

--- momentum.py
+++ momentum.py
@@ -1,13 +1,18 @@
 import csv
 
 
-def main():
-	#ticker = input("which stock would you like an opinion of?")
-	#will make call to api with this ticker in the future
+class Momentum:
+
+	def __init__(self, filename):
+
+		self.filename = filename
+
+	def findMA():
+
 	lastTwenty = []
 	#we start with a 6 column csv file: date, close,volume, open, high, low
-	filename = "aapl.csv"
-	v = open(filename)
+	companyCSV = self.filename
+	v = open(companyCSV)
 	r = csv.reader(v)
 	row0 = next(r)
 	row0.append("20 DMA")
@@ -17,20 +22,18 @@ def main():
 	for i in range(21):
 		if i == 0: continue
 		sumlast20 += float(exampledata[i][1])
-	print("The moving average is: ", sumlast20/20, "Last Close was: ", exampledata[1][1])
-	if sumlast20/20 >= float(exampledata[1][1]):
+	twentyDMA = sumLast20/20
+	dataTuple = (twentyDMA, float(exampledata[1][1])
+	return dataTuple
+
+	def printResults():
+
+		MAthenPrice = findMA()
+		print("The moving average is: ", MAthenPrice[0], "Last Close was: ", MAthenPrice[1])
+	if MAthenPrice[0] >= MAthenPrice[1]:
 		print("The price is below the 20 day moving average, we do not reccommend this company.")
 	else:
 		print("The price is above the 20 day moving average, it is a viable purchase.")
-'''
-	sumLast20 = 0
-	for i in next(r) in range(0,20):
-		#sumLast20 += item[1]
-		print(item[i])
-	#for item in r:
-'''
-
-
 
 
 if __name__ == "__main__": main()
