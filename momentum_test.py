966159319e0920814e5bca2d36e8d6c0abb25f5b
jjh68069 <h.jason0@gmail.com>
2015-11-11 14:15:13 -0700

    Update to automated testing for momentum file

--- momentum_test.py
+++ momentum_test.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
 import unittest
-import testproc
+import momentum
 
 class MomentumTestCase(unittest.TestCase):
 
-	def
+	def test_init(self):
+		filename ="aapl.csv"
+		p = momentum.StockAnalysis(filename)
+		
+	def test_findMA(self):
+		filename = "aapl.csv"
+		p = momentum.StockAnalysis(filename)
+
+	def test_printResults(self):
+		filename = "aapl.csv"
+		p = momentum.StockAnalysis(filename)
+		assertEqual
