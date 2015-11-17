c63b90ac4bb4648485bd8e67dd3cf2f9b59da8bc
callahan890 <Andrew.Callahan@colorado.edu>
2015-11-16 23:12:59 -0700

    Got updateData.py to make the database

--- Database/updateData.py
+++ Database/updateData.py
@@ -10,6 +10,9 @@ import urllib.request
 import csv
 
 conn = sqlite3.connect('stocks.db')
+c = conn.cursor()
+
+
 
 stock_ticker = ["MMM", "ABT", "ABBV", "ACN", "ACE", "ADBE", "ADT", "AES", "AET", "AMG", "AFL", "A", "GAS", "APD", "ARG", "AKAM", "AA", "ALXN", "ATI", "ALLE", "AGN", "ADS", "ALL", "ALTR", "MO", "AMZN", "AEE", "AAL", "AEP", "AXP", "AIG", "AMT", "AMP", "ABC", "AME", "AMGN", "APH", "APC", "ADI", "ANTM", "AON", "APA", "AIV", "AAPL", "AMAT", "ADM", "AIZ", "T", "ADSK", "ADP", "AN", "AZO", "AVGO", "AVB", "AVY", "BHI", "BLL", "BAC", "BK", "BCR", "BAX", "BBT", "BDX", "BBBY", "BRK-B", "BBY", "BIIB", "BLK", "BA", "BWA", "BXP", "BSX", "BMY", "BRCM", "BF-B", "CHRW", "CA", "CVC", "COG", "CAM", "CPB", "COF", "CAH", "KMX", "CCL", "CAT", "CBG", "CBS", "CELG", "CNP", "CTL", "CERN", "CF", "CHK", "CVX", "CMG", "CB", "CI", "XEC", "CINF", "CTAS", "CSCO", "C", "CTXS", "CLX", "CME", "CMS", "COH", "KO", "CCE", "CTSH", "CL", "CMCSA", "CMA", "CSC", "CAG", "COP", "CNX", "ED", "STZ", "GLW", "COST", "CCI", "CSX", "CMI", "CVS", "DHI", "DHR", "DRI", "DVA", "DE", "DLPH", "DAL", "XRAY", "DVN", "DO", "DTV", "DFS", "DISCA", "DG", "DLTR", "D", "DOV", "DOW", "DPS", "DTE", "DD", "DUK", "DNB", "ETFC", "EMN", "ETN", "EBAY", "ECL", "EIX", "EW", "EA", "EMC", "EMR", "ENDP", "ESV", "ETR", "EOG", "EQT", "EFX", "EQIX", "EQR", "ESS", "EL", "ES", "EXC", "EXPE", "EXPD", "ESRX", "XOM", "FFIV", "FB", "FDO", "FAST", "FDX", "FIS", "FITB", "FSLR", "FE", "FISV", "FLIR", "FLS", "FLR", "FMC", "FTI", "F", "FOSL", "BEN", "FCX", "FTR", "GME", "GPS", "GRMN", "GM", "GD", "GE", "GGP", "GIS", "GPC", "GNW", "GILD", "GS", "GT", "GOOGL", "GWW", "HRB", "HAL", "HBI", "HOG", "HAR", "HRS", "HIG", "HAS", "HCA", "HCP", "HCN", "HP", "HSY", "HES", "HPQ", "HD", "HON", "HRL", "HSP", "HST", "HCBK", "HUM", "HBAN", "ITW", "IR", "INTC", "ICE", "IBM", "IFF", "IP", "IPG", "INTU", "ISRG", "IVZ", "IRM", "JEC", "JNJ", "JCI", "JOY", "JPM", "JNPR", "KSU", "K", "GMCR", "KEY", "KMB", "KIM", "KMI", "KLAC", "KSS", "KRFT", "KR", "LB", "LLL", "LH", "LRCX", "LM", "LEG", "LEN", "LUK", "LVLT", "LLY", "LNC", "LLTC", "LMT", "L", "LOW", "LYB", "MTB", "MAC", "M", "MNK", "MRO", "MPC", "MAR", "MMC", "MLM", "MAS", "MA", "MAT", "MKC", "MCD", "MHFI", "MCK", "MJN", "MDT", "MRK", "MET", "KORS", "MCHP", "MU", "MSFT", "MHK", "TAP", "MDLZ", "MON", "MNST", "MCO", "MS", "MOS", "MSI", "MUR", "MYL", "NDAQ", "NOV", "NAVI", "NTAP", "NFLX", "NWL", "NFX", "NEM", "NWSA", "NEE", "NKE", "NI", "NE", "NBL", "JWN", "NSC", "NTRS", "NOC", "NRG", "NUE", "NVDA", "ORLY", "OXY", "OMC", "OKE", "ORCL", "OI", "PCAR", "PH", "PDCO", "PAYX", "PNR", "PBCT", "POM", "PEP", "PKI", "PRGO", "PFE", "PCG", "PM", "PSX", "PNW", "PXD", "PBI", "PCL", "PNC", "PPG", "PPL", "PX", "PCP", "PCLN", "PFG", "PG", "PGR", "PLD", "PRU", "PSA", "PEG", "PHM", "PVH", "QEP", "QRVO", "QCOM", "PWR", "DGX", "RL", "RRC", "RTN", "O", "RHT", "REGN", "RF", "RSG", "RAI", "RHI", "ROK", "COL", "ROP", "ROST", "RCL", "R", "SNDK", "SCG", "HSIC", "SLB", "SCHW", "SNI", "STX", "SEE", "SRE", "SHW", "SIAL", "SPG", "SWKS", "SLG", "SJM", "SNA", "SO", "LUV", "SWN", "SE", "STJ", "SWK", "SPLS", "SBUX", "HOT", "STT", "SRCL", "SYK", "STI", "SYMC", "SYY", "TROW", "TGT", "TEL", "TE", "TGNA", "THC", "TDC", "TSO", "TXN", "TXT", "TMO", "TIF", "TWX", "TWC", "TJX", "TMK", "TSS", "TSCO", "RIG", "TRV", "TRIP", "FOXA", "TYC", "TSN", "USB", "UA", "UNP", "UPS", "URI", "UTX", "UNH", "UHS", "UNM", "URBN", "VLO", "VAR", "VTR", "VRSN", "VZ", "VRTX", "VFC", "VIAB", "V", "VNO", "VMC", "WMT", "WBA", "DIS", "WM", "WAT", "WEC", "WFC", "WDC", "WU", "WY", "WHR", "WFM", "WMB", "WYN", "WYNN", "XEL", "XRX", "XLNX", "XL", "XYL", "YHOO", "YUM", "ZBH", "ZTS"]
 
@@ -19,18 +22,29 @@ stock_ticker = ["MMM", "ABT", "ABBV", "ACN", "ACE", "ADBE", "ADT", "AES", "AET",
 working_directory = os.getcwd()
 file_path = os.path.join(working_directory + '/data')
 
-for i in stock_ticker[0:1]:
-    #print (i)
-    #please note that this is soham's api key
-    url = 'https://www.quandl.com/api/v3/datasets/WIKI/{0}/data.csv?api_key=eFWztrj8CjJUa4zjQ2Mz'.format(i)
-    print (url)
-    #save_location = file_path{0}.format(i)
-    response = urllib.request.urlopen(url)
+for i in stock_ticker[0:2]:
+	print (i) #Company being added to database
+	url = 'https://www.quandl.com/api/v3/datasets/WIKI/{0}/data.csv?start_date=2015-10-30?api_key=API_KEY_GOES_HERE'.format(i)
 
-    #csv_file = csv.reader(response)
-    #print(csv_file)
+	print ("saving file")
+	urllib.request.urlretrieve(url, 'test.csv')
+	print ("file saved")
 
-    csv_file = response.read()
-    print (csv_file)
 
-    response.close()
+	print ("create table")
+	c.execute("CREATE TABLE '%s' (Date TEXT Primary Key, Open INT, High INT, Low INT, Close INT, Volume INT, Ex_Dividend INT, Split_Ratio INT, Adj_Open INT, Adj_High INT, Adj_Low INT, Adj_Close INT, Adj_Volume INT);"%i)
+	conn.commit()
+
+	with open('test.csv', 'r') as f:
+		reader = csv.reader(f)
+		myList = list(reader)
+		for row in myList:
+			print (row) #Row of CSV file added to Database
+			c.executemany("INSERT INTO {} VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);".format(i), (row,))
+			conn.commit()
+			print ("commit")
+	#csv_file = response.read()
+	#print (csv_file)
+	f.closed
+	#response.close()
+conn.close()
