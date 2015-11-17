5af4afe0ebff7d19dfaea134309264a8a63c85fa
callahan890 <Andrew.Callahan@colorado.edu>
2015-11-16 23:24:00 -0700

    updateData.py now makes a database

--- momentum_test.py
+++ momentum_test.py
@@ -1,19 +1,48 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#J-Dog
+#Come at me
 
 import unittest
 import momentum
 
 class MomentumTestCase(unittest.TestCase):
 
-	def test_init(self):
-		filename ="aapl.csv"
-		p = momentum.StockAnalysis(filename)
-		
-	def test_findMA(self):
-		filename = "aapl.csv"
-		p = momentum.StockAnalysis(filename)
+    @classmethod
+    def setUpClass(cls):
+        pass
 
-	def test_printResults(self):
-		filename = "aapl.csv"
-		p = momentum.StockAnalysis(filename)
-		assertEqual
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_init(self):
+        filename = "nflx.csv"
+        p = momentum.StockAnalysis(filename)
+        self.assertEqual(p.filename, filename, "'text' does not match input")
+
+    # Add Your Test Cases Here...
+    def testFindMA(self):
+        filename = "nflx.csv"
+        p = momentum.StockAnalysis(filename)
+        self.assertIsInstance(p.findMA(),tuple, "Error in return type")
+
+    def testPrintResults(self):
+        filename = "nflx.csv"
+        p = momentum.StockAnalysis(filename)
+        self.assertIsNone(p.printResults(), "Error in return type")
+
+
+
+    
+# Main: Run Test Cases
+if __name__ == '__main__':
+    unittest.main()
+
